86d55cb9b7633f2f7d4d3ad0f855e509e81abc07
Gary <suen601@gmail.com>
2023-02-17 23:36:11 +0000

    updated gitignore for ssl cert updated load_raw_stock_to_db_sqlorm to avoid the case when data.Date already in format of datetime.date

--- constant/config_template.py
+++ constant/config_template.py
@@ -1,5 +1,17 @@
 #db_login_info
 
+login =''
+password = ''
+db_url = ''
+schema = ''
+path_to_ssl =''
+
+
+#db_login_info
 db_login= {
-    'root':'mysql+pymysql://username":password@url:port/db',
+    'root':'mysql+pymysql://root:root@localhost:3307/db',
+    'azure':f'mysql+pymysql://{login}'
+            f':{password}'
+            f'@{db_url}/{schema}'
+            f'?ssl_ca={path_to_ssl}'
 }
